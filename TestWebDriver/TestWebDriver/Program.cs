8a780c933d6ce3432d46f66a7da6a9d3bfc4c5bf
jiangzeng01 <49379637+jiangzeng01@users.noreply.github.com>
2020-10-22 23:29:08 -0700

    Add source code

---
+++ TestWebDriver/TestWebDriver/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Edge.SeleniumTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWebDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new EdgeOptions();
+            options.UseChromium = true;
+            var driver = new EdgeDriver(options);
+            driver.Navigate().GoToUrl(@"https://www.microsoft.com/");
+            driver.Manage().Window.Maximize();
+            Console.ReadKey();
+        }
+    }
+}
